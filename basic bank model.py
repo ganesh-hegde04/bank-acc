e058ec2179761335814f41ce54fdcfcd57ea4974
ganesh-hegde04 <hegdeganesh504@gmail.com>
2024-06-22 18:28:57 +0530

    basic bank model.py

--- basic bank model.py
+++ basic bank model.py
@@ -2,10 +2,7 @@ class Account():
   def __init__(self,owner,balance=0):
     self.owner=owner
     self.balance=balance
-  def __deposite__(self,dep):
-    self.balance=self.balance +dep
-    print(f"RUPEES {dep} HAS BEEN DEPOSITED")
-  def __withdrwal__(self,wd):
+  def __deposite__(self,dep):`
       if self.balance>=wd:
          self.balance=self.balance-wd
          print(f" withdrawing {wd} rupees")
@@ -14,10 +11,3 @@ class Account():
   def __str__(self):
      return f"owner:{self.owner} \nbalance:{self.balance}"
 a=Account("ganesh",400)
-print(a)
-a.__deposite__(200)
-print(a)
-a.__withdrwal__(600)
-print(a)
-a.__withdrwal__(1)
-print(a)
